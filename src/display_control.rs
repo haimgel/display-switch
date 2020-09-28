f06381af86a9536798d9ce38186e126f64ee6ed7
Haim Gelfenbeyn <haim@g8n.me>
2020-09-28 08:47:21 -0400

    Add an index to display names if the names are not unique.

--- src/display_control.rs
+++ src/display_control.rs
@@ -6,12 +6,22 @@
 use crate::configuration::{Configuration, SwitchDirection};
 use crate::input_source::InputSource;
 use ddc_hi::{Ddc, Display};
+use std::collections::HashSet;
 
 /// VCP feature code for input select
 const INPUT_SELECT: u8 = 0x60;
 
-fn display_name(display: &Display) -> String {
-    format!("'{}'", display.info.id)
+fn display_name(display: &Display, index: Option<usize>) -> String {
+    if let Some(index) = index {
+        format!("'{} #{}'", display.info.id, index)
+    } else {
+        format!("'{}'", display.info.id)
+    }
+}
+
+fn are_display_names_unique(displays: &[Display]) -> bool {
+    let mut hash = HashSet::new();
+    !displays.iter().all(|display| hash.insert(display_name(display, None)))
 }
 
 pub fn log_current_source() {
@@ -20,8 +30,9 @@ pub fn log_current_source() {
         error!("Did not detect any DDC-compatible displays!");
         return;
     }
-    for mut display in displays {
-        let display_name = display_name(&display);
+    let unique_names = are_display_names_unique(&displays);
+    for (index, mut display) in displays.into_iter().enumerate() {
+        let display_name = display_name(&display, if unique_names { None } else { Some(index + 1) });
         match display.handle.get_vcp_feature(INPUT_SELECT) {
             Ok(raw_source) => {
                 let source = InputSource::from(raw_source.value());
@@ -40,8 +51,9 @@ pub fn switch(config: &Configuration, switch_direction: SwitchDirection) {
         error!("Did not detect any DDC-compatible displays!");
         return;
     }
-    for mut display in displays {
-        let display_name = display_name(&display);
+    let unique_names = are_display_names_unique(&displays);
+    for (index, mut display) in displays.into_iter().enumerate() {
+        let display_name = display_name(&display, if unique_names { None } else { Some(index + 1) });
         if let Some(input) = config.configuration_for_monitor(&display_name).source(switch_direction) {
             debug!("Setting display {} to {}", display_name, input);
             match display.handle.set_vcp_feature(INPUT_SELECT, input.value()) {
