7bf11582c8c02c17bfdd8b657d7a1fc9a477bcb4
Haim Gelfenbeyn <haim@hageltech.com>
2021-10-17 20:08:31 -0400

    Re-attach the console on Windows

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@
 extern crate log;
 
 use anyhow::Result;
+use winapi::um::wincon::{AttachConsole, ATTACH_PARENT_PROCESS};
 
 mod app;
 mod configuration;
@@ -17,9 +18,19 @@ mod logging;
 mod platform;
 mod usb;
 
+/// On Windows, re-attach the console, if parent process has the console. This allows
+/// to see the log output when run from the command line.
+fn attach_console() {
+    if cfg!(windows) {
+        unsafe {
+            AttachConsole(ATTACH_PARENT_PROCESS);
+        }
+    }
+}
+
 fn main() -> Result<()> {
+    attach_console();
     let app = app::App::new()?;
     app.run()?;
-
     Ok(())
 }
