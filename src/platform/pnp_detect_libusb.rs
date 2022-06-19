4c7375edfa2d2263bc59dd9b9fc5bc2395df1658
Adam Johns <robert.adam.johns@gmail.com>
2022-06-18 20:47:34 -0500

    revert unnecessary change

--- src/platform/pnp_detect_libusb.rs
+++ src/platform/pnp_detect_libusb.rs
@@ -44,7 +44,9 @@ impl PnPDetectLibusb {
                 );
 
             loop {
-                context.handle_events(None).unwrap();
+                if let Err(err) = context.handle_events(None) {
+                    error!("Error during USB errors handling: {:?}", err)
+                };
             }
         } else {
             // This should never happen: hotplug is supported on Linux and MacOS both.
